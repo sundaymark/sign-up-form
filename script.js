a3cb8b10c9862beedd39ad01a5b114535aa623df
sundaymark <sundayukpai1122@gmail.com>
2023-08-08 01:37:52 +0100

    created the script file and selected the inputs

---
+++ script.js
@@ -0,0 +1,8 @@
+const firstName = document.getElementsByClassName('fname');
+const lastName = document.getElementsByClassName('lname');
+const email = document.getElementsByClassName('email');
+const phoneNumber = document.getElementsByClassName('pnumber');
+const passWord = document.getElementsByClassName('password');
+const confirmPassword = document.getElementsByClassName('error');
+const btn = document.getElementsByClassName('btn');
+
