03a76108b5b5470eb846630316a80b6f36d972fd
mikenath223 <michgoldennathan17@gmail.com>
2023-08-20 10:23:45 +0100

    Update form style and implement password check

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
- const button = document.querySelector('button').addEventListener(onclick,function(){
-    const password = document.querySelector('password').value;
-    const confirmpassword = document.querySelector('confirmpassword').value;
+const password = document.body.querySelector(".password");
+const confirmPassword = document.body.querySelector(".confirmpassword");
+const errorText = document.body.querySelector(".error-text");
 
-    if(password.value != confirmpassword.value){
-        alert('passwords did not match try again.');
-        return false
-    }else if(password.value == confirmpassword.value){
-        alert('password match')
-    }
-    return true;
-})
+const checkPasswords = () => {
+  if (password.value === confirmPassword.value) {
+    password.classList.remove("error");
+    confirmPassword.classList.remove("error");
+    errorText.style.visibility = "hidden";
+  } else {
+    password.classList.add("error");
+    confirmPassword.classList.add("error");
+    errorText.style.visibility = "initial";
+  }
+};
 
+password.addEventListener("input", checkPasswords);
+confirmPassword.addEventListener("input", checkPasswords);
